1dae187cc9890673c0ce2a677527eafde3c276b2
Xavi Garcia <masvidal.xavi@gmail.com>
2024-10-03 11:27:54 +0200

    Searching when button is clicked. Remembering the company when going back to the page.

--- 04-frameworks/01-react/src/list.tsx
+++ 04-frameworks/01-react/src/list.tsx
@@ -8,22 +8,49 @@ interface MemberEntity {
 }
 
 export const ListPage: React.FC = () => {
-  const [members, setMembers] = React.useState<MemberEntity[]>([]);
 
-  React.useEffect(() => {
-    fetch(`https://api.github.com/orgs/lemoncode/members`)
+  const getStoredOrganization = () => {
+
+    let storedOrganization = window.sessionStorage.getItem("storedOrganization");
+
+    if (storedOrganization == undefined) {
+      return "lemoncode";
+    }
+
+    return storedOrganization;
+  }
+
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [organization, setOrganization] = React.useState<string>(getStoredOrganization());
+
+  const searchOrg = () => {
+    fetch(`https://api.github.com/orgs/${organization}/members`)
       .then((response) => response.json())
       .then((json) => setMembers(json));
+
+    window.sessionStorage.setItem("storedOrganization", organization);
+  }
+
+  React.useEffect(() => {
+    searchOrg();
   }, []);
 
   return (
     <>
       <h2>Hello from List page</h2>+{" "}
+      <div>
+        <h3>Organization: {organization}</h3>
+        <input
+            value={organization}
+            onChange={(e) => setOrganization(e.target.value)}
+        ></input>
+        <button onClick={searchOrg}>Search</button>
+      </div>
       <div className="list-user-list-container">
         <span className="list-header">Avatar</span>
         <span className="list-header">Id</span>
         <span className="list-header">Name</span>
-        {members.map((member) => (
+        {members.length > 0 && members.map((member) => (
           <>
             <img src={member.avatar_url} />
             <span>{member.id}</span>
