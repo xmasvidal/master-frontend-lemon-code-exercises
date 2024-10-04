074ba27947c7c1198af6d0e7cd75bf57c5f849cd
Xavi Garcia <masvidal.xavi@gmail.com>
2024-10-04 12:14:45 +0200

    added pagintation using the github api links from the response header

--- 04-frameworks/01-react/src/list.tsx
+++ 04-frameworks/01-react/src/list.tsx
@@ -7,6 +7,13 @@ interface MemberEntity {
   avatar_url: string;
 }
 
+interface PaginationLink {
+  first: string,
+  previous: string,
+  next: string,
+  last: string
+}
+
 export const ListPage: React.FC = () => {
 
   const getStoredOrganization = () => {
@@ -22,18 +29,96 @@ export const ListPage: React.FC = () => {
 
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const [organization, setOrganization] = React.useState<string>(getStoredOrganization());
+  const [paginationLink, setPaginationLink] = React.useState<PaginationLink>();
 
-  const searchOrg = () => {
-    fetch(`https://api.github.com/orgs/${organization}/members`)
-      .then((response) => response.json())
-      .then((json) => setMembers(json));
+  function parsePaginationLinks(header: string): PaginationLink {
+    const links: PaginationLink = {
+      first: undefined,
+      previous: undefined,
+      next: undefined,
+      last: undefined
+    };
 
-    window.sessionStorage.setItem("storedOrganization", organization);
+    if (header === null) {
+      return links;
+    }
+  
+    const parts = header.split(', ');
+  
+    parts.forEach(part => {
+      const section = part.split(';');
+      if (section.length !== 2) {
+        return;
+      }
+  
+      const url = section[0].replace(/<(.*)>/, '$1').trim();
+      const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+  
+      if (name === 'first') {
+        links.first = url;
+      } else if (name === 'prev') {
+        links.previous = url;
+      } else if (name === 'next') {
+        links.next = url;
+      } else if (name === 'last') {
+        links.last = url;
+      }
+    });
+  
+    return links;
   }
 
+  const searchPage = (link :string) => {
+    fetch(link)
+      .then((response) => {
+        let link = response.headers.get('Link');
+        setPaginationLink(parsePaginationLinks(link));
+        return response.json();
+      })
+      .then((json) => setMembers(json));
+  }
+
+  const searchOrganization = () => {
+    searchPage(`https://api.github.com/orgs/${organization}/members?page=1&per_page=5`)
+    window.sessionStorage.setItem("storedOrganization", organization);
+  }
+  
+
   React.useEffect(() => {
-    searchOrg();
+    searchOrganization();
   }, []);
+
+  const searchFirstPage = () => {
+    searchPage(paginationLink.first);
+  }
+
+  const searchPreviousPage = () => {
+    searchPage(paginationLink.previous);
+  }
+
+  const searchNextPage = () => {
+    searchPage(paginationLink.next);
+  }
+
+  const searchLastPage = () => {
+    searchPage(paginationLink.last);
+  }
+
+  const hasFirstPage = () => {
+    return paginationLink && paginationLink.first === undefined;
+  }
+
+  const hasPreviousPage = () => {
+    return paginationLink && paginationLink.previous === undefined;
+  }
+
+  const hasNextPage = () => {
+    return paginationLink && paginationLink.next === undefined;
+  }
+
+  const hasLastPage = () => {
+    return paginationLink && paginationLink.last === undefined;
+  }
 
   return (
     <>
@@ -44,7 +129,13 @@ export const ListPage: React.FC = () => {
             value={organization}
             onChange={(e) => setOrganization(e.target.value)}
         ></input>
-        <button onClick={searchOrg}>Search</button>
+        <button onClick={searchOrganization}>Search</button>
+      </div>
+      <div className="paginator">
+        <button onClick={searchFirstPage} disabled={hasFirstPage()}>first</button>
+        <button onClick={searchPreviousPage} disabled={hasPreviousPage()}>previous</button>
+        <button onClick={searchNextPage} disabled={hasNextPage()}>next</button>
+        <button onClick={searchLastPage} disabled={hasLastPage()}>last</button>
       </div>
       <div className="list-user-list-container">
         <span className="list-header">Avatar</span>
