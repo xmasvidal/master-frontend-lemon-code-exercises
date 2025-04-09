f893d2d3f8189deaa99ae39c063fe1f0f3c335a8
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-04-09 15:17:54 +0200

    pointing to the Rick and Morty API

--- 06-rest-api/laboratory/vite.config.ts
+++ 06-rest-api/laboratory/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://rickandmortyapi.com/',
       '/thumbnails': 'http://localhost:3000',
     },
   },
