7ca7a65a7d32ec2bbae652d1bff6e8c4834178be
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-04-12 00:05:01 +0200

    added bestSentence and edit funcionality

--- 06-rest-api/laboratory/src/pods/character/character.validations.ts
+++ 06-rest-api/laboratory/src/pods/character/character.validations.ts
@@ -1,19 +1,9 @@
 import { ValidationSchema, Validators } from '@lemoncode/fonk';
 import { createFormikValidation } from '@lemoncode/fonk-formik';
-import { minNumber } from '@lemoncode/fonk-min-number-validator';
 
 const validationSchema: ValidationSchema = {
   field: {
-    name: [Validators.required],
-    description: [Validators.required],
-    rating: [
-      {
-        validator: minNumber,
-        customArgs: { minValue: 3 },
-      },
-    ],
-    address: [Validators.required],
-    city: [Validators.required],
+    name: [Validators.required]
   },
 };
 
