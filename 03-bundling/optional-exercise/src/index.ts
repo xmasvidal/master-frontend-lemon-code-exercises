a5551b89ef4d3ce90f7e570abbcb95671480ddf8
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-05-06 10:23:37 +0200

    initial commit

---
+++ 03-bundling/optional-exercise/src/index.ts
@@ -0,0 +1,1 @@
+import "./my-styles.scss";
---
+++ 03-bundling/optional-exercise/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    app: "./index.ts"
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)s$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], 
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      scriptLoading: "blocking"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  devServer: {
+    port: 8080,
+  },
+};
