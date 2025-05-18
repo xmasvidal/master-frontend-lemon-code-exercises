1910b0aff57ef6526b63b21cb4f3387ccc22613a
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-05-18 22:13:36 +0200

    minor improvements

--- 04-frameworks/02-angular/mandatory-exercise/app/src/app/layout/private-header/private-header.component.ts
+++ 04-frameworks/02-angular/mandatory-exercise/app/src/app/layout/private-header/private-header.component.ts
@@ -3,10 +3,11 @@ import { PrivateMenuComponent } from '../private-menu/private-menu.component';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { Subscription } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-private-header',
-  imports: [PrivateMenuComponent],
+  imports: [PrivateMenuComponent, MatButtonModule],
   templateUrl: './private-header.component.html',
   styleUrl: './private-header.component.scss'
 })
