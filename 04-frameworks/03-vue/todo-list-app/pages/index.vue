250e47b6e7896f5bbc8993f2d82420bd62656eb9
Xavi Masvidal <masvidal.xavi@gmail.com>
2025-05-21 16:03:25 +0200

    initial commit

---
+++ 04-frameworks/03-vue/todo-list-app/pages/index.vue
@@ -0,0 +1,11 @@
+<template> 
+    <header>
+        <h1>TODO APP</h1>
+    </header>
+    <main></main>
+    <footer>
+        <p>Made with ❤️ by <a href="https://github.com/xmasvidal">xmasvidal</a></p>
+        <p>Powered by <a href="https://vuejs.org/">Vue.js</a></p>
+        <p>Using <a href="https://nuxtjs.org/">Nuxt.js</a></p>
+    </footer>
+</template>
